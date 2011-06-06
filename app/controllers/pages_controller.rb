91e583c5ab0b4c48037e61dd0f7846f47b15d3f3
Danigb <danigb@gmail.com>
2011-06-06 16:10:52 +0200

    Page caching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   layout 'pages'
 
   def index
-
+    age = 60 * 60 # 60 minutes
+    response.headers['Cache-Control'] = "public, max-age=#{age}"
   end
 end
