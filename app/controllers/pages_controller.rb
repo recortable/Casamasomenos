0ffc7e62ba88efb98b3ecd04784ae55711875a59
Danigb <danigb@gmail.com>
2011-06-07 12:01:37 +0200

    empezamos con amazon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   expose(:article) { Article.find(1) }
 
   def index
-    age = 24 * 60 * 60 # 24 horas
-    response.headers['Cache-Control'] = "public, max-age=#{age}"
+    #age = 24 * 60 * 60 # 24 horas
+    #response.headers['Cache-Control'] = "public, max-age=#{age}"
   end
 end
