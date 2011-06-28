d9670cab2a906231eb0857754e1d77ba3036252c
Danigb <danigb@gmail.com>
2011-06-28 21:00:06 +0200

    browser

--- app/controllers/categories_controller.rb
+++
@@ -1,8 +0,0 @@
-class CategoriesController < ApplicationController
-  respond_to :html
-  expose(:category) { Category.find(params[:id])}
-
-  def show
-
-  end
-end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,4 @@
 
 class Category
-
   CATEGORIES = ['page', 'area', 'concept', 'strategy', 'typological', 'management', 'participatory', 'experience']
-
-  def self.find(name)
-
-  end
-
-  def initialize
-
-  end
 end
