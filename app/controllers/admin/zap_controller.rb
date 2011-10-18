fe36d51ff32bcae861b2f19d9cb9c04314e19af7
Danigb <danigb@gmail.com>
2011-10-18 18:57:13 +0200

    Trying to make it work on production: remove class attributes... ejem...

--- app/controllers/admin/zap_controller.rb
+++ app/controllers/admin/zap_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ZapController < Admin::ApplicationController
-  expose(:zap_class) { "#{@@zap_name}".camelcase.constantize }
-  expose(:zap) { send(@@zap_name) }
-  expose(:zaps) { send(@@zap_name.pluralize) }
+  expose(:zap_class) { "#{zap_resource_name}".camelcase.constantize }
+  expose(:zap) { send(zap_resource_name) }
+  expose(:zaps) { send(zap_resource_name.pluralize) }
 
   def index
     respond_with zaps
@@ -16,16 +16,18 @@ class Admin::ZapController < Admin::ApplicationController
   end
 
   def update
-    data = params[@@zap_name]
+    data = params[zap_resource_name]
     #render text: data.inspect
     result = resource.update_attributes(data) ? 'updated' : 'update_error'
-    flash[:notice] = t("#{@@zap_name}.flash.#{result}")
+    flash[:notice] = t("#{zap_resource_name}.flash.#{result}")
     respond_with zap, location: [:admin, zap]
   end
 
 
   protected
   def self.resource(resource_name)
-    @@zap_name = resource_name.to_s
+    define_method 'zap_resource_name' do
+      resource_name.to_s
+    end
   end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+
+User.create(:name => 'admin', :email => 'admin@casamasomenos.net')
+User.create(:name => 'anonymous', :email => 'anonymous@casamasomenos.net')
+
