4393247f978b31198f467b7ead203769d8e44307
Danigb <danigb@gmail.com>
2011-10-18 19:09:20 +0200

    Joder con los seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 
-User.create(:name => 'admin', :email => 'admin@casamasomenos.net')
-User.create(:name => 'anonymous', :email => 'anonymous@casamasomenos.net')
+User.create!(name: 'admin', email: 'admin', password: 'secret', password_confirmation: 'secret')
+User.create!(name: 'anonymous', email: 'anonymous', password: 'secret', password_confirmation: 'secret')
+Article.create!(title: 'Welcome', author_id: 1, state: 'published', category_id: 1)
 
